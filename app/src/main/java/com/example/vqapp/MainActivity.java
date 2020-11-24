19350f52164cc02c6d56dc829c6605441745799e
nzuerch <nelson.zurcher@epfl.ch>
2020-11-24 16:52:37 +0100

    Test incroyable 2

--- app/src/main/java/com/example/vqapp/MainActivity.java
+++ app/src/main/java/com/example/vqapp/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //j'ai un gros zizi
+        int a = 0;
     }
 }
