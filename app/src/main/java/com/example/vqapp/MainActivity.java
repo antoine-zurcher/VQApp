00369ef34f34881264b3e310b6e1219c946368e1
antoine-zurcher <antoinezr98@gmail.com>
2020-12-05 10:42:18 +0100

    Merge branch 'master' of C:\Users\antoine\CloudStation\EPFL\Master 3\Lab on App\VQApp with conflicts.

--- app/src/main/java/com/example/vqapp/MainActivity.java
+++ app/src/main/java/com/example/vqapp/MainActivity.java
@@ -1,99 +1,142 @@
 package com.example.vqapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentContainer;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
-import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
-import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.TextView;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.support.common.FileUtil;
+import org.tensorflow.lite.support.image.ImageProcessor;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.image.ops.ResizeOp;
+import org.tensorflow.lite.task.vision.classifier.Classifications;
+import org.tensorflow.lite.task.vision.classifier.ImageClassifier;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private static final String TAG_FRAGMENT = "TAG";
-    private String state = "live";
-    private int requestCode = 100;
-
-
-
+    ImageView imageView;
+    Button btn_camera, btn_gallery;
+    TextView tv_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, requestCode);
+        imageView = (ImageView) findViewById(R.id.image);
+        btn_camera = (Button) findViewById(R.id.btn_camera);
+        btn_gallery = (Button) findViewById(R.id.btn_gallery);
+        tv_result = (TextView) findViewById(R.id.tv_result);
 
-        Fragment someFragment = new LiveFragment();
-        FragmentManager mManager = getSupportFragmentManager();
-        mManager.popBackStack();
-        FragmentTransaction mTransaction = mManager.beginTransaction();
+        btn_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //To take picture from camera
 
-        mTransaction.replace(R.id.fragment, someFragment ); // give your fragment container id in first parameter
-        mTransaction.addToBackStack(null);  // if written, this transaction will be added to backstack
-        mTransaction.commit();
-    }
+                Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(takePicture, 0);//zero can be replaced with any action code
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) { switch(item.getItemId()) {
-
-        case R.id.fixed:
-            if(state == "live"){
-                Fragment someFragment = new FixedFragment();
-                FragmentManager mManager = getSupportFragmentManager();
-                mManager.popBackStack();
-                FragmentTransaction mTransaction = mManager.beginTransaction();
-
-                mTransaction.replace(R.id.fragment, someFragment ); // give your fragment container id in first parameter
-                mTransaction.addToBackStack(null);  // if written, this transaction will be added to backstack
-                mTransaction.commit();
-
-                state = "fixed";
             }
-            Toast.makeText(this, "Fixed", Toast.LENGTH_LONG).show();
-            return(true);
-        case R.id.live:
-            if(state == "fixed"){
-                Fragment someFragment = new LiveFragment();
-                FragmentManager mManager = getSupportFragmentManager();
-                mManager.popBackStack();
-                FragmentTransaction mTransaction = mManager.beginTransaction();
+        });
+        btn_gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //To pick photo from gallery
 
-                mTransaction.replace(R.id.fragment, someFragment ); // give your fragment container id in first parameter
-                mTransaction.addToBackStack(null);  // if written, this transaction will be added to backstack
-                mTransaction.commit();
-
-                state = "live";
+                Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(pickPhoto, 1);//one can be replaced with any action code
             }
-            Toast.makeText(this, "Live", Toast.LENGTH_LONG).show();
-            return(true);
+        });
 
     }
-        return(super.onOptionsItemSelected(item));
+
+    private MappedByteBuffer loadModelFile(Activity activity) throws IOException {
+        AssetFileDescriptor fileDescriptor = activity.getAssets().openFd("mobilenet_v2_1.0_224_quant.tflite");
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch (requestCode) {
+            case 0:
+                if (resultCode == RESULT_OK) {
+                    Bundle extras = imageReturnedIntent.getExtras();
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+                    imageView.setImageBitmap(imageBitmap);
+
+                    Interpreter tflite;
+                    try {
+                        tflite = new Interpreter(loadModelFile(this));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    String result = "result";
+                    tv_result.setText(result);
+                }
+
+                break;
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    imageView.setImageURI(selectedImage);
+                }
+                break;
+        }
+    }
+
+
+    private String doInference(Bitmap imageBitmap) throws IOException {
+        // Initialization code
+        // Create an ImageProcessor with all ops required. For more ops, please
+        // refer to the ImageProcessor Architecture section in this README.
+        ImageProcessor imageProcessor =
+                new ImageProcessor.Builder()
+                        .add(new ResizeOp(224, 224, ResizeOp.ResizeMethod.BILINEAR))
+                        .build();
+
+        // Create a TensorImage object. This creates the tensor of the corresponding
+        // tensor type (uint8 in this case) that the TensorFlow Lite interpreter needs.
+        TensorImage tImage = new TensorImage(DataType.UINT8);
+
+        // Analysis code for every frame
+        // Preprocess the image
+        tImage.load(imageBitmap);
+        //tImage = imageProcessor.process(tImage);
+
+
+        // Initialization
+        ImageClassifier.ImageClassifierOptions options = ImageClassifier.ImageClassifierOptions.builder().setMaxResults(1).build();
+        ImageClassifier imageClassifier = ImageClassifier.createFromFileAndOptions(this, "mobilenet_v2_1.0_224_quant.tflite", options);
+
+        // Run inference
+        List<Classifications> results = imageClassifier.classify(tImage);
+        return "text";
     }
 
 
