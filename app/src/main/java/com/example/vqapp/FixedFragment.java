d99852046a7ae588b25b89a74fa58ef86067f45b
antoine-zurcher <antoinezr98@gmail.com>
2020-12-05 17:18:58 +0100

    Added Classification for fixed fragment

--- app/src/main/java/com/example/vqapp/FixedFragment.java
+++ app/src/main/java/com/example/vqapp/FixedFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.SystemClock;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,18 +15,37 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import org.tensorflow.lite.examples.classification.tflite.Classifier;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Logger;
 
 import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link FixedFragment#newInstance} factory method to
+ * Use the {@link FixedFragment} factory method to
  * create an instance of this fragment.
  */
 public class FixedFragment extends Fragment {
 
     private Button btn_camera, btn_gallery;
     private ImageView imageView;
+    private Classifier classifier;
+    private static final String TAG = "FixedFragment";
+    private long lastProcessingTimeMs;
+    private int sensorOrientation = 0;
+    private List<Classifier.Recognition> results;
+    private TextView tv_output;
+
+    private Classifier.Model model = Classifier.Model.QUANTIZED_MOBILENET;
+    private Classifier.Device device = Classifier.Device.CPU;
+    private int numThreads = 1;
+
 
     public FixedFragment() {
         // Required empty public constructor
@@ -47,6 +67,8 @@ public class FixedFragment extends Fragment {
         btn_gallery = (Button) rootView.findViewById(R.id.btn_gallery_fixed);
 
         imageView = (ImageView) rootView.findViewById(R.id.imageView);
+
+        tv_output = (TextView) rootView.findViewById(R.id.output_fixed);
 
         btn_camera.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,6 +102,9 @@ public class FixedFragment extends Fragment {
                     Bundle extras = imageReturnedIntent.getExtras();
                     Bitmap imageBitmap = (Bitmap) extras.get("data");
                     imageView.setImageBitmap(imageBitmap);
+
+                    results = runClassification(imageBitmap);
+                    showClassificationResults(results);
                 }
 
                 break;
@@ -87,8 +112,44 @@ public class FixedFragment extends Fragment {
                 if (resultCode == RESULT_OK) {
                     Uri selectedImage = imageReturnedIntent.getData();
                     imageView.setImageURI(selectedImage);
+
+                    try {
+                        // Convert URI to Bitmap
+                        Bitmap imageBitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), selectedImage);
+                        results = runClassification(imageBitmap);
+                        showClassificationResults(results);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
                 break;
         }
+    }
+
+    private void showClassificationResults(List<Classifier.Recognition> results) {
+        Classifier.Recognition recognition = results.get(0);
+        tv_output.setText(recognition.getTitle());
+    }
+
+    private List<Classifier.Recognition> runClassification(Bitmap imageBitmap) {
+        try {
+            classifier = Classifier.create(getActivity(), model, device, numThreads);
+            Log.v(TAG, "created classifier");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (classifier != null) {
+            final long startTime = SystemClock.uptimeMillis();
+            results =
+                    classifier.recognizeImage(imageBitmap, sensorOrientation);
+            lastProcessingTimeMs = SystemClock.uptimeMillis() - startTime;
+
+            Log.e(TAG, "time "+String.valueOf(lastProcessingTimeMs));
+
+            return results;
+        }
+
+        return null;
     }
 }
