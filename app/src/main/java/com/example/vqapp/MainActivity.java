fa7b91ecd80d847379a02ae2d0b71fa4d9ac4709
antoine-zurcher <antoinezr98@gmail.com>
2021-01-06 10:31:13 +0100

    added vqa model

--- app/src/main/java/com/example/vqapp/MainActivity.java
+++ app/src/main/java/com/example/vqapp/MainActivity.java
@@ -19,10 +19,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.vqapp.ml.FinalModel;
+
+import org.tensorflow.lite.DataType;
 import org.tensorflow.lite.examples.classification.tflite.Classifier;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import java.io.IOException;
 import java.util.List;
@@ -43,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView mModeTextView;
     private TextView mOutput;
     private Button mCompute;
+    private EditText mQuestion;
 
     Fragment mLiveFragment;
     Fragment mFixedFragment;
@@ -82,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
         mModeTextView = findViewById(R.id.mode);
         mOutput = findViewById(R.id.output);
         mCompute = findViewById(R.id.compute);
+        mQuestion = findViewById(R.id.question);
 
         mCompute.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("LongLogTag")
@@ -89,31 +96,50 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 FragmentManager manager = getSupportFragmentManager();
                 Fragment fragment = manager.findFragmentById(R.id.fragment);
-                if(state == State.LIVE){
-                    try {
-                        liveHandler.postDelayed(liveRunnable = new Runnable() {
-                            @Override
-                            public void run() {
-                                liveHandler.postDelayed(liveRunnable, DELAY_MODEL);
-                                Log.e("in handler", "method compute model");
-                                try {
-                                    mOutput.setText(mModel.runModel(mLiveFragment.getActivity()));
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                }
+
+                String question = mQuestion.getText().toString();
+
+                if(question.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please enter a question", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    // check if question is not too long
+                    String[] words= question.split(" ");
+                    int nbWords = words.length;
+
+                    if(nbWords > 16){
+                        Toast.makeText(MainActivity.this, "The maximum length of the question is 16 words", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        if(state == State.LIVE){
+                            try {
+                                liveHandler.postDelayed(liveRunnable = new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        liveHandler.postDelayed(liveRunnable, DELAY_MODEL);
+                                        Log.e("in handler", "method compute model");
+                                        try {
+                                            mOutput.setText(mModel.runModel(mLiveFragment.getActivity(), mLiveFragment.getContext(), question));
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                }, DELAY_MODEL);
+                            }catch (Exception e){
+                                Log.e("error in adding handler: ", e.getMessage());
                             }
-                        }, DELAY_MODEL);
-                    }catch (Exception e){
-                        Log.e("error in adding handler: ", e.getMessage());
+                        }
+                        else if(state == State.FIXED){
+
+                            try {
+                                mOutput.setText(mModel.runModel(mFixedFragment.getActivity(), mFixedFragment.getContext(), question));
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
                     }
                 }
-                else if(state == State.FIXED){
-                    try {
-                        mOutput.setText(mModel.runModel(mFixedFragment.getActivity()));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
+
             }
         });
     }
